2fcac80147e6d78775054264c11309b0e16110ea
Riki Nakamura <riki.nakamura.edu@gmail.com>
2024-02-18 14:44:50 +0900

    Add comment

--- examples/cds/cds.ino
+++ examples/cds/cds.ino
@@ -1,16 +1,21 @@
 #include <CanSatSchool.h>
 
+// 光センサの出力ピンを指定
 #define PIN_CDS A1
 
+// 光センサを宣言
 CdS cds(PIN_CDS);
 
 void setup()
 {
+    // シリアル通信 (Arduino-PC) を初期化
     Computer::init(9600);
 
-    // Wire(Arduino-I2C)の初期化
+    // Wire (Arduino-I2C) を初期化
+    // 光センサは I2C を使用するためこれが必要
     Wire.begin();
 
+    // 光センサを初期化
     cds.init();
 
     delay(300);
@@ -18,7 +23,9 @@ void setup()
 
 void loop()
 {
+    // 光センサの値を取得してシリアルモニタに表示
     print(cds.read());
 
+    // 1s 待つ
     delay(1000);
 }
