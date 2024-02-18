3351f59f14f88b1394ee5ddf92a2a8a7c40fd00f
Riki Nakamura <riki.nakamura.edu@gmail.com>
2024-02-18 16:15:26 +0900

    Update example

--- examples/servo_motor/servo_motor.ino
+++ examples/servo_motor/servo_motor.ino
@@ -19,32 +19,28 @@ void setup()
 
 void loop()
 {
-    static uint16_t loop_count = 0;
+    // サーボモーターを1度に回転
+    // 角度は1度から180度まで指定できる
+    servo.rotateTo(1);
+    // 500ms 待つ
+    delay(500);
+    // サーボモーターの角度を取得してシリアルモニタに表示
+    print(servo.read());
 
-    if (loop_count % 10 == 0) {
-        print(F("Move Up!"));
-        // 1度から180度まで 30度ずつ回転
-        for (uint8_t angle = 1; angle <= 180; angle += 30) {
-            // サーボモーターを指定した角度に回転
-            servo.rotateTo(angle);
-            delay(100);
-            // サーボモーターの角度を取得してシリアルモニタに表示
-            print(F("angle:"), servo.read());
-            delay(500);
-        }
-    }
-    if (loop_count % 10 == 5) {
-        print(F("Move Down!"));
-        // 180度から1度まで 1度ずつ回転
-        for (uint8_t angle = 180; angle > 0; --angle) {
-            // サーボモーターを指定した角度に回転
-            servo.rotateTo(angle);
-            // サーボモーターの角度を取得してシリアルモニタに表示
-            print(F("angle:"), servo.read());
-            delay(10);
-        }
-    }
+    // サーボモーターを90度に回転
+    servo.rotateTo(90);
+    // 500ms 待つ
+    delay(500);
+    // サーボモーターの角度を取得してシリアルモニタに表示
+    print(servo.read());
 
-    ++loop_count;
+    // サーボモーターを180度に回転
+    servo.rotateTo(180);
+    // 500ms 待つ
+    delay(500);
+    // サーボモーターの角度を取得してシリアルモニタに表示
+    print(servo.read());
+
+    // 1s 待つ
     delay(1000);
 }
