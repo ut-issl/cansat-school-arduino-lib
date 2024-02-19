f96acaee11c0fe3a8fd1796c5029c5c160f30a4f
Riki Nakamura <riki.nakamura.edu@gmail.com>
2024-02-19 23:18:22 +0900

    Add logger

---
+++ src/utility/logger.cpp
@@ -0,0 +1,80 @@
+#include "logger.hpp"
+
+namespace Utility
+{
+
+    void Logger::setDebug(const bool& is_debug)
+    {
+        is_debug_ = is_debug;
+    }
+
+    bool Logger::enableComputer(const unsigned long& baud_rate)
+    {
+        Device::Computer::init(baud_rate);
+        use_computer_ = true;
+        return true;
+    }
+
+    bool Logger::enableSDCard(const String& log_file_name, uint8_t ss_pin)
+    {
+        if (!Device::SDCard::init(ss_pin)) {
+            return false;
+        }
+        log_file_name_ = log_file_name;
+        use_sd_card_ = true;
+        return true;
+    }
+
+    void Logger::info(const String& message) const
+    {
+        if (use_computer_) {
+            Device::Computer::print(timestamp_(), F("[INFO]"), message);
+        }
+        if (use_sd_card_) {
+            Device::SDCard::write(log_file_name_, timestamp_() + F(" [INFO] ") + message);
+        }
+    }
+
+    void Logger::error(const String& message) const
+    {
+        if (use_computer_) {
+            Device::Computer::print(timestamp_(), F("[ERROR]"), message);
+        }
+        if (use_sd_card_) {
+            Device::SDCard::write(log_file_name_, timestamp_() + F(" [ERROR] ") + message);
+        }
+    }
+
+    void Logger::warning(const String& message) const
+    {
+        if (use_computer_) {
+            Device::Computer::print(timestamp_(), F("[WARNING]"), message);
+        }
+        if (use_sd_card_) {
+            Device::SDCard::write(log_file_name_, timestamp_() + F(" [WARNING] ") + message);
+        }
+    }
+
+    void Logger::debug(const String& message) const
+    {
+        if (is_debug_) {
+            if (use_computer_) {
+                Device::Computer::print(timestamp_(), F("[DEBUG]"), message);
+            }
+            if (use_sd_card_) {
+                Device::SDCard::write(log_file_name_, timestamp_() + F(" [DEBUG] ") + message);
+            }
+        }
+    }
+
+    String Logger::timestamp_() const
+    {
+        double sec = float(millis()) / 1000.0;
+        char buffer[9];
+        dtostrf(sec, 7, 2, buffer);
+        return String(buffer);
+    }
+
+    Logger logger;
+
+}  // namespace Utility
--- src/utility/logger.hpp
+++ src/utility/logger.hpp
@@ -13,7 +13,10 @@ namespace Utility
         Logger() = default;
         ~Logger() = default;
 
-        void init();
+        void setDebug(const bool& is_debug = true);
+
+        bool enableComputer(const unsigned long& baud_rate = 9600);
+        bool enableSDCard(const String& log_file_name = "log.txt", uint8_t ss_pin = 10);
 
         void info(const String& message) const;
         void error(const String& message) const;
@@ -22,10 +25,16 @@ namespace Utility
 
       private:
 
-        bool use_computer_ = true;
+        String timestamp_() const;
+
+        bool is_debug_ = false;
+
+        bool use_computer_ = false;
         bool use_sd_card_ = false;
 
         String log_file_name_;
     };
+
+    extern Logger logger;
 
 }  // namespace Utility
