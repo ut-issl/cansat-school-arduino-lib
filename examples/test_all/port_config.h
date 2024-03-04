96d1ff39ae1acb8fe1aba2d75f4224777a23b204
Riki Nakamura <riki.nakamura.edu@gmail.com>
2024-03-04 22:53:13 +0900

    Add test_all

---
+++ examples/test_all/port_config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define PIN_SERVO 4        // サーボモーターの入力ピン
+#define PIN_DC_MOTER_1 5   // DCモーターの入力ピン
+#define PIN_DC_MOTER_2 6   // DCモーターの入力ピン
+#define PIN_CAMERA_SS 7    // カメラの SS ピン
+#define PIN_GPS_TX 8       // GPS の TX ピン
+#define PIN_USD_TRIGGER 9  // 超音波距離センサーのトリガーピン
+#define PIN_SD_SS 10       // SDカードの SS ピン
+#define PIN_CDS A1         // 光センサの出力ピン
+#define PIN_MIC A2         // マイクの出力ピン
+#define PIN_USD_ECHO 17    // 超音波距離センサーのエコーピン
---
+++ examples/test_all/test_all.ino
@@ -0,0 +1,112 @@
+#include <CanSatSchool.h>
+
+#include "port_config.h"
+
+// 気圧・温湿度計を宣言
+BaroThermoHygrometer bth;
+// 光センサを宣言
+CdS cds{PIN_CDS};
+// 超音波距離センサーを宣言
+UltrasonicDistanceSensor distance_sensor{PIN_USD_TRIGGER, PIN_USD_ECHO};
+// IMU の宣言
+IMU imu;
+// マイクを宣言
+Microphone microphone{PIN_MIC};
+// GPS を宣言
+GPSReceiver gps{PIN_GPS_TX};
+
+// サーボモーターを宣言
+ServoMotor servo{PIN_SERVO};
+// DCモーターを宣言
+DCMotor dc_motor{PIN_DC_MOTER_1, PIN_DC_MOTER_2};
+
+// カメラを宣言
+Camera camera{PIN_CAMERA_SS};
+
+void setup()
+{
+    // ロガーを初期化
+    logger.enableComputer();
+    String file_name = "log.txt";  // 記録するファイル名
+    logger.enableSDCard(file_name, PIN_SD_SS);
+    logger.setDebug();
+
+    // Wire (Arduino-I2C) を初期化
+    Wire.begin();
+    // SPI を初期化
+    SPI.begin();
+
+    // 気圧・温湿度計を初期化
+    bth.init();
+    // 光センサを初期化
+    cds.init();
+    // 超音波距離センサーを初期化
+    distance_sensor.init();
+    // IMU を初期化
+    imu.init();
+    // マイクを初期化
+    microphone.init();
+    // GPS を初期化
+    gps.init();
+
+    // サーボモーターを初期化
+    servo.init();
+    // DCモーターを初期化
+    dc_motor.init();
+
+    // カメラを初期化
+    camera.init();
+}
+
+void loop()
+{
+    // 気圧・温湿度計の値を取得して記録
+    bth.read().print();
+    delay(500);
+
+    // 光センサの値を取得して記録
+    logger.info(F("[CdS]"), cds.read());
+    delay(500);
+
+    // 超音波距離センサーの値を取得して記録
+    logger.info(F("Distance:"), distance_sensor.read(), "[m]");
+    delay(500);
+
+    // IMU の値を取得して記録
+    imu.read().print();
+    delay(500);
+
+    // マイクの値を取得して記録
+    logger.info(microphone.read());
+    delay(500);
+
+    // GPS から取得した情報を記録
+    gps.read().print();
+    delay(500);
+
+    // サーボモーターを動かす
+    servo.rotateTo(1);  // 1度に回転
+    delay(500);
+    logger.info(F("[Servo]"), servo.read(), "[deg]");
+    servo.rotateTo(90);  // 90度に回転
+    delay(500);
+    logger.info(F("[Servo]"), servo.read(), "[deg]");
+    servo.rotateTo(180);  // 180度に回転
+    delay(500);
+    logger.info(F("[Servo]"), servo.read(), "[deg]");
+    delay(500);
+
+    // DCモーターを動かす
+    dc_motor.forward();  // 正転
+    delay(5000);
+    dc_motor.free();  // 空転
+    delay(5000);
+    dc_motor.reverse();  // 逆転
+    delay(5000);
+    dc_motor.brake();  // 停止
+    delay(500);
+
+    // カメラで写真を撮影してSDカードに保存する
+    camera.takePictureAndSaveAs(String(millis()) + ".jpg");
+    delay(500);
+}
