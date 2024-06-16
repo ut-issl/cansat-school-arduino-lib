f0021aa6fa0eadcca21b1deaa4a5b7bb3d7dc721
Riki Nakamura <riki.nakamura.edu@gmail.com>
2024-06-16 10:00:10 +0900

    Add comment

--- examples/test_imu/test_imu.ino
+++ examples/test_imu/test_imu.ino
@@ -28,6 +28,17 @@ void loop()
     // auto gyro = imu.readGyro();  // 角速度
     // auto mag = imu.readMag();    // 地磁気
 
+    // x, y, z それぞれの値を取り出したい場合
+    // float acc_x = acc.x;  // 加速度 x 方向
+    // float acc_y = acc.y;  // 加速度 y 方向
+    // float acc_z = acc.z;  // 加速度 z 方向
+    // float gyro_x = gyro.x;  // 角速度 x 方向
+    // float gyro_y = gyro.y;  // 角速度 y 方向
+    // float gyro_z = gyro.z;  // 角速度 z 方向
+    // float mag_x = mag.x;    // 地磁気 x 方向
+    // float mag_y = mag.y;    // 地磁気 y 方向
+    // float mag_z = mag.z;    // 地磁気 z 方向
+
     // 1s 待つ
     delay(1000);
 }
